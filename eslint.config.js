a9dbacdf2463111520c7c7f5ae78c5dfedc19e11
Gilang Heavy <gilangheavy@gmail.com>
2025-11-27 10:07:28 +0700

    fix: remove sample storybook stories to fix lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import js from '@eslint/js';
 import globals from 'globals';
@@ -89,5 +89,5 @@ export default [{ignores: ['dist']}, {
     ],
     'react/prop-types': 'off',
   },
-}, ...storybook.configs["flat/recommended"]];
+}, ...storybook.configs['flat/recommended']];
 
